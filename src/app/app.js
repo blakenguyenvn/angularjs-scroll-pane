f4d1a03aa2145ab83a75c4450bd8d8ccf79d05bd
tuan.nguyen <tuan.nguyen@opswat.com>
2018-06-25 08:22:40 +0700

    Finish sources

--- src/app/app.js
+++ src/app/app.js
@@ -4,6 +4,22 @@
   /**
    * Main NYTimes app
    */
-  var MainApp = angular.module('MainApp', []);
+  var MainApp = angular.module('MainApp', [
+		 'ngScrollbars'
+  ]);
+
+  MainApp.config(function(ScrollBarsProvider) {
+    //== Default configuration for ScrollBars
+    ScrollBarsProvider.defaults = {
+			autoHideScrollbar: false,
+			scrollInertia: 500,
+			theme: 'dark',
+			axis: 'y',
+			advanced: {
+				updateOnContentResize: true
+			}
+		};
+
+  });
 
 })()
--- src/app/controllers/main.js
+++ src/app/controllers/main.js
@@ -14,26 +14,109 @@
       }]
     );
 
-  MainControllerInit.$inject = ['$scope', '$sce', 'NewsStore', 'NewsActions'];
+  MainControllerInit.$inject = ['$scope', '$sce', '$http', '$q'];
 
-  function MainControllerInit($scope, $sce, NewsStore, NewsActions){
-
-    $scope.news_store = NewsStore.init({
-      api_key: '092b8aac63e3484ea21a5245dfe44acc'
-    });
-
-    $scope.news_actions = NewsActions;
-    /**
-     *  Define load more news functions
-     */
-    $scope.loadMoreNews = function(){
-      $scope.news_actions.loadMoreNews();
-    };
+  function MainControllerInit($scope, $sce, $http, $q) {
 
     /**
-     * Scroll Pane Config
+     * News Pane Object
+     * Note: You shoould move this object to AngularJS Service when use in project
      */
+     $scope.newsPanel = {
+         data: {},
+         currentPage: 0,
+         isLoading: false,
+         requestConfig: {
+             api_key: '092b8aac63e3484ea21a5245dfe44acc',
+             nation: 'singapore',
+             page: 0
+         },
+         scrollbarConfig: {
+             setHeight: 'auto',
+             mouseWheel: {
+	                preventDefault: false
+                },
+    		callbacks: {
+    		onTotalScrollOffset: 0,
+    		onTotalScroll: function() {
+    			$scope.newsPanel.loadMore();
+    		},
+    	 	onCreate: function() {
+     			var height = '90vh';
+    	      		$(this).css('max-height', height);
+    		    }
+    		}
+        }
+     };
 
+     //== News Panel: load data function
+     $scope.newsPanel.loadData = function () {
+
+         var self = this;
+
+         //== Check if API Key is wrong
+         if (self.requestConfig.api_key == null) {
+
+             console.error('API key is wrong.');
+             return false;
+
+         } else {
+
+             var deferred = $q.defer();
+             var self = this;
+
+             //== Start loading
+             self.isLoading = true;
+
+             $http({
+                 method : 'GET',
+                 url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + "api-key=" + self.requestConfig.api_key + "&q=" + self.requestConfig.nation + "&page=" + self.requestConfig.page,
+                 async: false
+             })
+             .then(function successCallback(response) {
+
+                 if (response.data.status != undefined && response.data.status == 'OK') {
+
+                     self.data[self.requestConfig.page] = response.data.response.docs;
+                     console.log(self.data);
+                     self.currentPage = self.requestConfig.page;
+
+                     deferred.resolve(response);
+                 }
+                 //== Stop Loading
+                 self.isLoading = false;
+
+             }, function errorCallback(response) {
+
+                 //== Handle error and stop loading
+                 deferred.reject(error);
+                 self.isLoading = false;
+
+            });
+
+            //== Return Promise
+            return deferred.promise;
+         }
+     };
+
+     //== News Panel: load More Function
+     $scope.newsPanel.loadMore = function () {
+         var self = this;
+
+         self.requestConfig.page = self.currentPage + 1;
+
+         self.loadData();
+     };
+
+     //== News panel Init function, we can config API key, first page or any init data
+     $scope.newsPanel.init = function() {
+         this.loadData();
+     }
+
+     /**
+      * Controller Run Init
+      */
+      $scope.newsPanel.init();
 
   };
 
